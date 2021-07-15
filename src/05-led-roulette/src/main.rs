3d77d1b2cc04b91f1dfbfd18b9b79b49307d871e
Waffle <waffle.lapkin@gmail.com>
2021-07-15 15:31:24 +0300

    5.4: update the code

--- src/05-led-roulette/src/main.rs
+++ src/05-led-roulette/src/main.rs
@@ -2,14 +2,20 @@
 #![no_main]
 #![no_std]
 
-use aux5::entry;
+use aux5::{entry, DelayMs, OutputSwitch};
 
 #[entry]
 fn main() -> ! {
-    let _y;
-    let x = 42;
-    _y = x;
+    let (mut delay, mut leds) = aux5::init();
 
-    // infinite loop; just so we don't leave this stack frame
-    loop {}
+    let mut half_period_ = 300_u16;
+    let half_period = volatile::Volatile::new(&mut half_period_);
+
+    loop {
+        leds[0].on().ok();
+        delay.delay_ms(half_period.read());
+
+        leds[0].off().ok();
+        delay.delay_ms(half_period.read());
+    }
 }
