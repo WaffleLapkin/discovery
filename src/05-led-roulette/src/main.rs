f450b0e3bab8695955a2bed374d69a7f1045fb40
Waffle <waffle.lapkin@gmail.com>
2021-07-15 17:03:25 +0300

    Figure out leds order

--- src/05-led-roulette/src/main.rs
+++ src/05-led-roulette/src/main.rs
@@ -6,16 +6,21 @@ use aux5::{entry, DelayMs, OutputSwitch};
 
 #[entry]
 fn main() -> ! {
-    let (mut delay, mut leds) = aux5::init();
+    // Test shows that leds go in order
+    // N, NE, E, SE, S, SW, W, NW
+    // (clockwise, staring from N)
+    let (mut delay, leds) = aux5::init();
 
     let mut half_period_ = 300_u16;
     let half_period = volatile::Volatile::new(&mut half_period_);
 
-    loop {
-        leds[0].on().ok();
+    for mut led in leds {
+        led.on().ok();
         delay.delay_ms(half_period.read());
 
-        leds[0].off().ok();
+        led.off().ok();
         delay.delay_ms(half_period.read());
     }
+
+    loop {}
 }
