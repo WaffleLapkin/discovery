aa4bd08350aaba68026f9c37b7979636b0d3b635
Waffle <waffle.lapkin@gmail.com>
2021-07-18 20:57:01 +0300

    11.7: initial reverse echo implementation

--- src/11-usart/src/main.rs
+++ src/11-usart/src/main.rs
@@ -6,6 +6,7 @@ use core::iter;
 
 #[allow(unused_imports)]
 use aux11::{entry, iprint, iprintln};
+use heapless::Vec;
 
 #[entry]
 fn main() -> ! {
@@ -19,15 +20,38 @@ fn main() -> ! {
         usart1.rdr.read().rdr().bits() as u8
     };
 
-    let send = |byte| {
+    let mut send = |byte| {
         // wait until it's safe to write to TDR
         while usart1.isr.read().txe().bit_is_clear() {}
+
+        //iprintln!(&mut _itm.stim[0], "Sending {}", byte as char);
 
         usart1.tdr.write(|w| w.tdr().bits(u16::from(byte)))
     };
 
+    // A buffer with 32 bytes of capacity
+    let mut buffer: Vec<u8, 32> = Vec::new();
+
     // Echo
-    iter::repeat_with(recv).for_each(send);
+    for byte in iter::repeat_with(recv) {
+        iprintln!(&mut _itm.stim[0], "Receiced {:x}", byte);
+
+        // Enter?
+        if byte == 0xd {
+            buffer.iter().rev().copied().for_each(&mut send);
+            buffer.clear();
+
+            continue;
+        }
+
+        if let Err(_) = buffer.push(byte) {
+            b"Error: Couldn't append input: Buffer overflow"
+                .iter()
+                .copied()
+                .for_each(&mut send);
+            buffer.clear();
+        }
+    }
 
     loop {}
 }
