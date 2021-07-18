56dde491108c914e97fa60da9545a8340a76b589
Waffle <waffle.lapkin@gmail.com>
2021-07-18 19:57:22 +0300

    11.6: echo server impl

--- src/11-usart/src/main.rs
+++ src/11-usart/src/main.rs
@@ -2,20 +2,32 @@
 #![no_main]
 #![no_std]
 
+use core::iter;
+
 #[allow(unused_imports)]
 use aux11::{entry, iprint, iprintln};
 
 #[entry]
 fn main() -> ! {
-    let (usart1, _mono_timer, _itm) = aux11::init();
+    let (usart1, _mono_timer, mut _itm) = aux11::init();
 
-    loop {
+    let recv = || {
         // Wait until there's data available
         while usart1.isr.read().rxne().bit_is_clear() {}
 
         // Retrieve the data
-        let _byte = usart1.rdr.read().rdr().bits() as u8;
+        usart1.rdr.read().rdr().bits() as u8
+    };
 
-        aux11::bkpt();
-    }
+    let send = |byte| {
+        // wait until it's safe to write to TDR
+        while usart1.isr.read().txe().bit_is_clear() {}
+
+        usart1.tdr.write(|w| w.tdr().bits(u16::from(byte)))
+    };
+
+    // Echo
+    iter::repeat_with(recv).for_each(send);
+
+    loop {}
 }
